d889c1a71c5731f9b1735497721b8af960fe7076
xqwert2002 <40173898+xqwert2002@users.noreply.github.com>
2018-06-24 23:47:33 +0800

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,14 @@
+n1=input("Enter a number")
+n2=input("Enter a number")
+n3=input("Enter a number")
+n4=input("Enter a number")
+n5=input("Enter a number")
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+result=n1+n2+n3+n4+n5
+print(result)
+list=[n1,n2,n3,n4,n5]
+print(max(list))
